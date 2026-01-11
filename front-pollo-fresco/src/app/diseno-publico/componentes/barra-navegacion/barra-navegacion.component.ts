7a8863ab361d3f6557a153267de517e1497c0648
Deyvisgova <deyvisgova@gmail.com>
2026-01-11 13:06:12 -0500

    Show basic landing content without material

--- front-pollo-fresco/src/app/diseno-publico/componentes/barra-navegacion/barra-navegacion.component.ts
+++ front-pollo-fresco/src/app/diseno-publico/componentes/barra-navegacion/barra-navegacion.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-
 @Component({
   selector: 'app-barra-navegacion',
   standalone: true,
-  imports: [MatButtonModule, MatToolbarModule],
+  imports: [],
   templateUrl: './barra-navegacion.component.html',
   styleUrl: './barra-navegacion.component.css'
 })
--- front-pollo-fresco/src/app/diseno-publico/componentes/pie-pagina/pie-pagina.component.ts
+++ front-pollo-fresco/src/app/diseno-publico/componentes/pie-pagina/pie-pagina.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { MatDividerModule } from '@angular/material/divider';
-
 @Component({
   selector: 'app-pie-pagina',
   standalone: true,
-  imports: [MatDividerModule],
+  imports: [],
   templateUrl: './pie-pagina.component.html',
   styleUrl: './pie-pagina.component.css'
 })
