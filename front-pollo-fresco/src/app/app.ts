fb245ed9de31672b6ce83e53e3881249a0dc67ff
Deyvisgova <deyvisgova@gmail.com>
2026-01-11 11:51:12 -0500

    Make root component standalone

--- front-pollo-fresco/src/app/app.ts
+++ front-pollo-fresco/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { InicioComponent } from './diseno-publico/paginas/inicio/inicio.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [InicioComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
