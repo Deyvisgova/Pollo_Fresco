ace90e95b3584b195fc2e31bb5c89f61e59fe394
Deyvisgova <deyvisgova@gmail.com>
2026-03-04 12:30:39 -0500

    Rediseña navbar con iconos y estilo diferenciado para ingresar

--- front-pollo-fresco/src/app/diseno-publico/estructura/barra-navegacion/barra-navegacion.ts
+++ front-pollo-fresco/src/app/diseno-publico/estructura/barra-navegacion/barra-navegacion.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-barra-navegacion',
   standalone: true,
-  imports: [MatButtonModule, MatToolbarModule, RouterLink, RouterLinkActive],
+  imports: [MatToolbarModule, MatIconModule, RouterLink, RouterLinkActive],
   templateUrl: './barra-navegacion.html',
   styleUrl: './barra-navegacion.css'
 })
