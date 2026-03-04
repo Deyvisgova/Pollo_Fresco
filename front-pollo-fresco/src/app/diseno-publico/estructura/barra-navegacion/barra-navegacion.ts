4982b9069948ffda576a8fffed19bfa945abd9a1
Deyvisgova <deyvisgova@gmail.com>
2026-03-04 11:08:32 -0500

    Aplica sticky footer con CSS Grid en layout principal

--- front-pollo-fresco/src/app/diseno-publico/estructura/barra-navegacion/barra-navegacion.ts
+++ front-pollo-fresco/src/app/diseno-publico/estructura/barra-navegacion/barra-navegacion.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-barra-navegacion',
   standalone: true,
-  imports: [MatButtonModule, MatToolbarModule, RouterLink, RouterLinkActive],
+  imports: [MatToolbarModule, RouterLink, RouterLinkActive],
   templateUrl: './barra-navegacion.html',
   styleUrl: './barra-navegacion.css'
 })
